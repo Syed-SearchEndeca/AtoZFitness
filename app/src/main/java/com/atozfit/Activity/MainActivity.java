1e8c9909f69c3258b8f4e7c63fcc9c5e778d683f
Syed <Syedghouse14@hotmail.com>
2021-05-30 11:54:21 +0530

    Commited

--- app/src/main/java/com/atozfit/Activity/MainActivity.java
+++ app/src/main/java/com/atozfit/Activity/MainActivity.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    String patnName=null;
+    String patnName = null;
 
     ProgressDialog mProgressBar;
 
@@ -37,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button viewBpBtn;
 
-    TextView systolicTxt =null;
+    TextView systolicTxt = null;
 
-    TextView diastolicTxt=null;
+    TextView diastolicTxt = null;
 
     ImageButton graphicalView;
 
@@ -47,10 +47,10 @@ public class MainActivity extends AppCompatActivity {
         return instance.getApplicationContext();
     }
 
-    public String retriveName(){
+    public String retriveName() {
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
-        atoZBPService= new AtoZBPService();
-        patnName =atoZBPService.retrievePatientName();
+        atoZBPService = new AtoZBPService();
+        patnName = atoZBPService.retrievePatientName();
         return patnName;
     }
 
@@ -64,24 +64,16 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String patientName = retriveName();
         startLoadData();
-        if(patientName!=null && !patientName.isEmpty()){
-           TextView tvName = (TextView)findViewById(R.id.welcome_txt);
-           tvName.setText("Welcome:" + " "+patientName);
-       }
+        if (patientName != null && !patientName.isEmpty()) {
+            TextView tvName = (TextView) findViewById(R.id.welcome_txt);
+            tvName.setText("Welcome:" + " " + patientName);
+        }
 
         addUserBtn = (Button) findViewById(R.id.add_user_btn);
         addUserBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openNewActivity(Registration.class);
-            }
-        });
-
-        graphicalView = (ImageButton) findViewById(R.id.GraphicalViewBtn);
-        graphicalView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewActivity(GraphViewer.class);
             }
         });
 
@@ -95,34 +87,43 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        viewBpBtn=(Button)findViewById(R.id.view_bp_btn);
+        viewBpBtn = (Button) findViewById(R.id.view_bp_btn);
         viewBpBtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick (View v){
-        openNewActivity(DisplayBP_Results.class);
-    }
-    });
+            @Override
+            public void onClick(View v) {
+                openNewActivity(DisplayBP_Results.class);
+            }
+        });
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-}
-    public void openNewActivity(Class className){
-        Intent intent = new Intent(this,className);
-        if(patnName!=null) {
+    }
+
+    public void openNewActivity(Class className) {
+        Intent intent = new Intent(this, className);
+        if (patnName != null) {
             intent.putExtra("patientName", patnName);
         }
         startActivity(intent);
     }
 
     public void startLoadData() {
-        atoZBPService= new AtoZBPService();
-        List<AtoZBPAttributes> bpAttributes=atoZBPService.retrieveBPData();
-        int size=bpAttributes.size();
-        if(size>0) {
+        atoZBPService = new AtoZBPService();
+        List<AtoZBPAttributes> bpAttributes = atoZBPService.retrieveBPData();
+        int size = bpAttributes.size();
+        if (size > 0) {
             systolicTxt = (TextView) findViewById(R.id.Systolic_txt);
             systolicTxt.setText("Systolic:" + " " + bpAttributes.get(size - 1).getSystolic());
             diastolicTxt = (TextView) findViewById(R.id.Diastolic_txt);
             diastolicTxt.setText("Diastolic:" + " " + bpAttributes.get(size - 1).getDiastolic());
+        } else if (size > 1) {
+            graphicalView = (ImageButton) findViewById(R.id.GraphicalViewBtn);
+            graphicalView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openNewActivity(GraphViewer.class);
+                }
+            });
         }
-    }
 
+    }
 }
 
